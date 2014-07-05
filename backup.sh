1267934c82bade548ff18c87eec589fe5a57851d
huyuehui <huyuehui@yahoo.com>
2014-07-05 19:01:54 +0800

    copy only .el files, ignore .elc files.

--- backup.sh
+++ backup.sh
@@ -4,9 +4,9 @@ set -e -x
 echo "Making a backup of your importantant configure files..."
 mkdir -p kernel
 cp -p /usr/src/linux/.config ./kernel/
-mkdir -p emacs
+mkdir -p emacs/el-get-init-files
 cp -p ~/.emacs.d/init.el ./emacs/
-cp -p -r ~/.emacs.d/el-get/el-get-init-files ./emacs
+cp -p ~/.emacs.d/el-get/el-get-init-files/*.el ./emacs/el-get-init-files/
 mkdir -p fvwm
-cp -p -r ~/.fvwm/* ./fvwm
+cp -p -r ~/.fvwm/* ./fvwm/
 echo "Backup Done!"
