58aabe10be0736c1beed0093dde7da11cf1803d0
huyuehui <huyuehui@yahoo.com>
2016-06-30 19:45:07 +0800

    add some Gentoo configuration files

--- backup.sh
+++ backup.sh
@@ -9,4 +9,10 @@ cp -p ~/.emacs.d/init.el ./emacs/
 cp -p ~/.emacs.d/el-get/el-get-init-files/*.el ./emacs/el-get-init-files/
 mkdir -p fvwm
 cp -p -r ~/.fvwm/* ./fvwm/
+mkdir -p /home/lucifer/work/projects/config/gentoo/etc/portage
+mkdir -p /home/lucifer/work/projects/config/gentoo/var/lib/portage
+pushd /etc/portage
+cp -p -r make.conf package.license package.use package.unmask repos.conf /home/lucifer/work/projects/config/gentoo/etc/portage/
+popd
+cp -p /var/lib/portage/world /home/lucifer/work/projects/config/gentoo/var/lib/portage/
 echo "Backup Done!"
