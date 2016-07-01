b83f13bd970eba7b6fd999d580dafb53111c370b
huyuehui <huyuehui@yahoo.com>
2016-07-01 18:46:04 +0800

    use rsync

--- backup.sh
+++ backup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e -x
 
+CONFIG_HOME=~/work/projects/config
+
 echo "Making a backup of your importantant configure files..."
 mkdir -p kernel
-cp -p /usr/src/linux/.config ./kernel/
+cp -p /usr/src/linux/.config $CONFIG_HOME/kernel/
 mkdir -p emacs/el-get-init-files
-cp -p ~/.emacs.d/init.el ./emacs/
-cp -p ~/.emacs.d/el-get/el-get-init-files/*.el ./emacs/el-get-init-files/
+cp -p ~/.emacs.d/init.el $CONFIG_HOME/emacs/
+rsync -av ~/.emacs.d/el-get/el-get-init-files/ $CONFIG_HOME/emacs/el-get-init-files/
 mkdir -p fvwm
-cp -p -r ~/.fvwm/* ./fvwm/
-mkdir -p /home/lucifer/work/projects/config/gentoo/etc/portage
-mkdir -p /home/lucifer/work/projects/config/gentoo/var/lib/portage
-pushd /etc/portage
-cp -p -r make.conf package.license package.use package.unmask repos.conf /home/lucifer/work/projects/config/gentoo/etc/portage/
-popd
-cp -p /var/lib/portage/world /home/lucifer/work/projects/config/gentoo/var/lib/portage/
+cp -p -r ~/.fvwm/* $CONFIG_HOME/fvwm/
+mkdir -p $CONFIG_HOME/gentoo/etc/portage
+rsync -av /etc/portage/ $CONFIG_HOME/gentoo/etc/portage/
+mkdir -p $CONFIG_HOME/gentoo/var/lib/portage
+cp -p /var/lib/portage/world $CONFIG_HOME/gentoo/var/lib/portage/
 echo "Backup Done!"
