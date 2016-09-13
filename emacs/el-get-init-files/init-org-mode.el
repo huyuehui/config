f4c08c821d678fa8e2d5b07cfe49dd384fb7c885
huyuehui <huyuehui@yahoo.com>
2016-09-13 17:12:55 +0800

    various update
    1. kernel -> 4.7.3
    2. add overlay to install visual-studio-code
    3. enable graphviz dot language support in org-babel

--- emacs/el-get-init-files/init-org-mode.el
+++ emacs/el-get-init-files/init-org-mode.el
@@ -1,5 +1,10 @@
-;;setup org-mode
+;;;setup org-mode
 (setq org-confirm-babel-evaluate nil)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((dot . t)))
+ 
 
 (setq org-publish-project-alist
       '(
@@ -12,11 +17,11 @@
          :publishing-function org-html-publish-to-html
          :auto-sitemap t
          )
-        ("org-static"
+        ("org-css"
          :base-directory "~/work/wiki/org/css"
          :base-extension "css"
          :publishing-directory "~/work/wiki/public_html/css"
          :publishing-function org-publish-attachment
          )
-        ("org" :components ("org-notes" "org-static"))
+        ("org" :components ("org-notes" "org-css"))
         ))
